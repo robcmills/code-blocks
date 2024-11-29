ce793c050d72992fc2c11e27fa215fce97e45510
Rob Mills <robcmills@gmail.com>
2024-11-28 19:26:22 -0800

    Add keydown handler to editable element and handle enter key for newlines
    This approach turned out to be more difficult than I hoped because
    of the odd behavior that must be emulated.
    
    The desired behavior is exhibited when the code element has the
    contenteditable attribute, and the user presses enter. The browser
    will append two new text nodes to the code element, one with a
    single newline character, and then another empty text node that
    appears on a new line, with cursor focus.
    
    When I attempted to replicate this behavior in a keydown handler,
    I got very different behavior, even though I seemed to have replicated
    the content of the child text nodes.
    
    Further experimentation would be required to continue making progress.
    But I think we will have better luck just moving the contenteditable
    attribute to the code element.

--- src/Editable.tsx
+++ src/Editable.tsx
@@ -1,7 +1,6 @@
+import { useState } from 'react'
 import { appendCode } from './appendCode'
 import './Editable.css'
-
-import { useState } from 'react'
 
 export function Editable() {
   const [value] = useState('')
@@ -9,7 +8,7 @@ export function Editable() {
 
   const onInput = (event: React.FormEvent<HTMLDivElement>) => {
     const target = event.target
-    console.log({ target })
+    // console.log({ target })
     if (!(target instanceof HTMLElement)) {
       return
     }
@@ -26,5 +25,47 @@ export function Editable() {
     }
   }
 
-  return <div className="Editable" contentEditable onInput={onInput} />
+  const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log('Editable', { event, key: event.key })
+    const selection = window.getSelection();
+    console.log({ selection })
+    const editableElement = selection?.focusNode;
+    console.log({ editableElement })
+    if (!editableElement) return
+    // if the editable element is a code element, prevent the default behavior
+    // (which is to append a new code element)
+    // and manaually emulate (best attempt) the behavior 
+    // when parent code element has editableContent attribute
+    // (which is to append new child text nodes)
+    const codeNode = editableElement.parentNode?.nodeName === 'CODE'
+      ? editableElement.parentNode
+      : null
+    if (event.key === 'Enter' && codeNode) {
+      console.log('Enter in code node')
+      event.preventDefault()
+
+      codeNode.appendChild(document.createTextNode('\n'))
+      const newTextNode = document.createTextNode('')
+      codeNode.appendChild(newTextNode)
+      // Create a new range
+      const range = document.createRange();
+      // Position the range at the end of the new text node
+      range.setStart(newTextNode, 0);
+      range.setEnd(newTextNode, 0);
+      // Get the selection object and update it
+      const selection = window.getSelection();
+      selection?.removeAllRanges();
+      selection?.addRange(range);
+    }
+  }
+
+  return (
+    <div
+      className="Editable"
+      contentEditable
+      onInput={onInput}
+      onKeyDown={onKeyDown}
+      tabIndex={0}
+    />
+  )
 }
--- src/appendCode.ts
+++ src/appendCode.ts
@@ -1,5 +1,8 @@
 export function appendCode(element: Element) {
   const codeElement = document.createElement('code')
   codeElement.appendChild(document.createElement('br'))
+  codeElement.onkeydown = (event) => {
+    console.log('codeElement', { event, key: event.key })
+  }
   element.appendChild(codeElement)
 }
